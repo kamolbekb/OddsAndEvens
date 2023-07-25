ce2afb14e58345195ed306d13428d31be8c9a80a
kamolbekb <118444680+kamolbekb@users.noreply.github.com>
2023-07-25 18:49:24 +0500

    Program Upgraded

--- OddsAndEvens/OddsAndEvens/Program.cs
+++ OddsAndEvens/OddsAndEvens/Program.cs
@@ -4,7 +4,50 @@
     {
         static void Main(string[] args)
         {
+            Random random = new Random();
 
+            int[][] jaggedArray = new int[random.Next(3, 5)][];
+
+            for (int i = 0; i < jaggedArray.Length; i++)
+            {
+                jaggedArray[i] = new int[(random.Next(3, 7))];
+            }
+
+            for (int i = 0; i < jaggedArray.Length; i++)
+            {
+
+                for (int j = 0; j < jaggedArray[i].Length; j++)
+                {
+                    jaggedArray[i][j] = random.Next(1, 10);
+                }
+            }
+            int odd = 0;
+            int even = 0;
+            for (int i = 0; i < jaggedArray.Length; i++)
+
+            {
+                for (int j = 0; j < jaggedArray[i].Length; j++)
+                {
+                    Console.Write($" {jaggedArray[i][j]}");
+                    if (jaggedArray[i][j] % 2 == 0)
+                    {
+                        even += jaggedArray[i][j];
+                    }
+                    else
+                    {
+                        odd += jaggedArray[i][j];
+                    }
+                }
+                Console.WriteLine();
+            }
+            if (even > odd)
+            {
+                Console.WriteLine($"Sum of evens is greater than odds, it equals {even}.");
+            }
+            else
+            {
+                Console.WriteLine($"Sum of odds is greater than evens, it equals {odd}.");
+            }
         }
     }
 }
